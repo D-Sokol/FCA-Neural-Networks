1208b25d6587829d82c54ab564738fd9842b486f
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-08-28 23:39:53 +0300

    Add functions for computing CV and CFC measures.

--- measures.cpp
+++ measures.cpp
@@ -3,3 +3,32 @@
 double Support(const FCA::Concept& c) {
     return static_cast<double>(c.ExtentSize()) / c.Extent().size();
 }
+
+double CVMeasure(const FCA::Concept& c, const FCA::Context& context) {
+    const auto& b = c.Intent();
+    double result = 0;
+    FCA::BitSet temp(context.AttrSize());
+    for (size_t i = 0; i < context.AttrSize(); ++i) {
+        if (b.test(i)) {
+            temp.set(i);
+            // May be cached for the different concepts in the real applications.
+            result += context.DrvtAttr(temp).count();
+            temp.reset(i);
+        }
+    }
+    return result * c.ExtentSize();
+}
+
+double CFCMeasure(const FCA::Concept& c, const FCA::Context& context) {
+    const auto& a = c.Extent();
+    double result = 0;
+    FCA::BitSet temp(context.AttrSize());
+    for (size_t i = 0; i < context.AttrSize(); ++i) {
+        temp.set(i);
+        auto y_prime = context.DrvtAttr(temp);
+        temp.reset(i);
+
+        result += (a & y_prime).count() / y_prime.count();
+    }
+    return result / c.ExtentSize();
+}
--- measures.h
+++ measures.h
@@ -4,5 +4,6 @@
 #include "fcai/src/fca_datastructures.h"
 
 double Support(const FCA::Concept& c);
-
+double CVMeasure(const FCA::Concept& c, const FCA::Context& context);
+double CFCMeasure(const FCA::Concept& c, const FCA::Context& context);
 #endif //FCANN_MEASURES_H
