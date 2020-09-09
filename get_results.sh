f0466b7449f800e764c4114365c193bb214d33aa
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-09-09 17:50:09 +0300

    Changed limits according to the cross-validation.

--- get_results.sh
+++ get_results.sh
@@ -11,8 +11,8 @@ datasets=( datasets/* )
 for dataset in "${datasets[@]}"; do
   output="results/full-1hl-$(basename "$dataset" ".txt").txt"
   echo "dataset,neurons,accuracy" >"$output"
-  for neurons in {1..20}; do
-    for i in {1..10}; do
+  for neurons in {1..40}; do
+    for i in {1..2}; do
       echo "Fully connected networks, neurons=${neurons}, iteration=${i}"
       result=( $(${EXEC} "${dataset}" full $neurons 2>/dev/null) )
       if [ $? -ne 0 ]; then break; fi
@@ -26,8 +26,8 @@ done
 for dataset in "${datasets[@]}"; do
   output="results/full-2hl-$(basename "$dataset" ".txt").txt"
   echo "dataset,neurons,accuracy" >"$output"
-  for neurons in {1..10}; do
-    for i in {1..10}; do
+  for neurons in {1..20}; do
+    for i in {1..2}; do
       echo "Fully connected networks, neurons=[${neurons}, ${neurons}], iteration=${i}"
       result=( $(${EXEC} "${dataset}" full $neurons $neurons 2>/dev/null) )
       if [ $? -ne 0 ]; then break; fi
@@ -41,9 +41,9 @@ done
 for dataset in "${datasets[@]}"; do
   output="results/full-min_supp-$(basename "$dataset" ".txt").txt"
   echo "dataset,min_supp,max_level,neurons,accuracy" >"$output"
-  for min_supp in $(seq 0.1 0.05 0.9); do
-    for max_level in {1..5}; do
-      for i in {1..10}; do
+  for min_supp in $(seq 0.1 0.1 0.9); do
+    for max_level in {1..4}; do
+      for i in {1..2}; do
         echo "min_support based network, min_supp=${min_supp}, max_level=${max_level}, iteration=${i}"
         result=( $(${EXEC} "${dataset}" min_supp $min_supp $max_level 2>/dev/null) )
         if [ $? -ne 0 ]; then break; fi
@@ -58,9 +58,9 @@ done
 for dataset in "${datasets[@]}"; do
   output="results/full-min_cv-$(basename "$dataset" ".txt").txt"
   echo "dataset,min_cv,max_level,neurons,accuracy" >"$output"
-  for min_cv in $(seq 0.1 0.05 0.9); do
-    for max_level in {1..5}; do
-      for i in {1..10}; do
+  for min_cv in $(seq 0.1 0.1 0.9); do
+    for max_level in {1..4}; do
+      for i in {1..2}; do
         echo "min_cv based network, min_cv=${min_cv}, max_level=${max_level}, iteration=${i}"
         result=( $(${EXEC} "${dataset}" cv $min_cv $max_level 2>/dev/null) )
         if [ $? -ne 0 ]; then break; fi
@@ -75,9 +75,9 @@ done
 for dataset in "${datasets[@]}"; do
   output="results/full-min_cfc-$(basename "$dataset" ".txt").txt"
   echo "dataset,min_cfc,max_level,neurons,accuracy" >"$output"
-  for min_cfc in $(seq 0.1 0.05 0.9); do
-    for max_level in {1..5}; do
-      for i in {1..10}; do
+  for min_cfc in $(seq 0.1 0.1 0.9); do
+    for max_level in {1..4}; do
+      for i in {1..2}; do
         echo "min_cfc based network, min_cfc=${min_cfc}, max_level=${max_level}, iteration=${i}"
         result=( $(${EXEC} "${dataset}" cfc $min_cfc $max_level 2>/dev/null) )
         if [ $? -ne 0 ]; then break; fi
