2e7684e1b847eb2fda0df48da3110e5815febf3c
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-09-14 00:36:12 +0300

    Allowed masking in ThetaSophia algorithm.

--- fca_algorithms.h
+++ fca_algorithms.h
@@ -2,13 +2,16 @@
 #define FCANN_FCA_ALGORITHMS_H
 
 #include <functional>
+#include <optional>
 #include <vector>
 #include "fcai/src/fca_datastructures.h"
 
 namespace FCA {
     using Predicate = std::function<bool(const FCA::Concept&, size_t)>;
 
-    std::vector<FCA::Concept> ThetaSophia(const FCA::Context&, Predicate keep_concept = [](const auto&, size_t){ return true; });
+    std::vector<FCA::Concept> ThetaSophia(const FCA::Context&,
+                                          Predicate keep_concept = [](const auto&, size_t){ return true; },
+                                          std::optional<std::vector<bool>> mask_objects = std::nullopt);
 
     std::pair<Context, std::vector<size_t>> ReadContext(std::istream& is);
 }
