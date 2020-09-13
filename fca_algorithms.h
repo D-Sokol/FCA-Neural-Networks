ec8b335623b0866d938d3306101d8b6b0a36b1e7
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-09-14 01:09:33 +0300

    Fix: create new formal context based on train data only for each cross validation split.

--- fca_algorithms.h
+++ fca_algorithms.h
@@ -8,9 +8,10 @@
 
 namespace FCA {
     using Predicate = std::function<bool(const FCA::Concept&, size_t)>;
+    const Predicate keep_all = [](const auto&, size_t){ return true; };
 
     std::vector<FCA::Concept> ThetaSophia(const FCA::Context&,
-                                          Predicate keep_concept = [](const auto&, size_t){ return true; },
+                                          Predicate keep_concept=keep_all,
                                           std::optional<std::vector<bool>> mask_objects = std::nullopt);
 
     std::pair<Context, std::vector<size_t>> ReadContext(std::istream& is);
