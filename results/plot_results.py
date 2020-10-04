a6ba1fce46d42bbb9d6dace787951430b32cfb0f
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-10-04 21:08:16 +0300

    Changed plot description.

--- results/plot_results.py
+++ results/plot_results.py
@@ -12,20 +12,20 @@ if __name__ == '__main__':
     for dataset in datasets:
         plt.figure(figsize=(12,7))
 
-        plt.title('Accuracy for dataset "{}"'.format(dataset))
-        plt.xlabel('Neurons')
-        plt.ylabel('Accuracy, %')
+        # plt.title('Accuracy for dataset "{}"'.format(dataset))
+        plt.xlabel('Количество нейронов')
+        plt.ylabel('Точность, %')
         plt.ylim(-5, 105)
         plt.yticks(range(0, 110, 10))
         plt.grid()
 
         data = pd.read_csv(filename_template.format(method='full-1hl', dataset=dataset))
         averaged = data.groupby('neurons').mean()
-        plt.plot(averaged.index, averaged['accuracy'], 'r--.', label='Fully connected (1 hidden layer)')
+        plt.plot(averaged.index, averaged['accuracy'], 'r--.', label='Dense (1 hidden layer)')
 
         data = pd.read_csv(filename_template.format(method='full-2hl', dataset=dataset))
         averaged = data.groupby('neurons').mean()
-        plt.plot(averaged.index, averaged['accuracy'], 'b--.', label='Fully connected (2 hidden layers)')
+        plt.plot(averaged.index, averaged['accuracy'], 'b--.', label='Dense (2 hidden layers)')
         # plt.legend()
 
         # ax = plt.gca().twiny()
@@ -34,15 +34,15 @@ if __name__ == '__main__':
         data = pd.read_csv(filename_template.format(method='full-min_supp', dataset=dataset))
         for max_level, subset in data.groupby('max_level'):
             averaged = subset.groupby('neurons').mean()
-            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'Min supp (lvl={max_level})')
+            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'Supp; $l_{{\\mathrm{{max}}}}={max_level}$')
         data = pd.read_csv(filename_template.format(method='full-min_cv', dataset=dataset))
         for max_level, subset in data.groupby('max_level'):
             averaged = subset.groupby('neurons').mean()
-            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'Min CV (lvl={max_level})')
+            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'CV; $l_{{\\mathrm{{max}}}}={max_level}$')
         data = pd.read_csv(filename_template.format(method='full-min_cfc', dataset=dataset))
         for max_level, subset in data.groupby('max_level'):
             averaged = subset.groupby('neurons').mean()
-            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'Min CFC (lvl={max_level})')
+            ax.plot(averaged.index, averaged['accuracy'], marker='*', label=f'CFC; $l_{{\\mathrm{{max}}}}={max_level}$')
         # ax.set_xlabel('Threshold for $\\mathcal{M}$-metric')
         # ax.set_xlim(0.0, 1.0)
         # ax.set_xticks(np.arange(0.0, 1.1, 0.1))
